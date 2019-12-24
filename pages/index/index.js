51db98e999ea773448be729b90a2d79088990d19
koushashadezh <kss9812@163.com>
2019-12-24 21:17:59 +0800

    12.24

---
+++ pages/index/index.js
@@ -0,0 +1,104 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    "fenhang": [{
+      'img': '../../imgs/paihang1.png',
+      'text': '科幻'
+    },
+    {
+      'img': '../../imgs/paihang2.png',
+      'text': '生活'
+    },
+    {
+      'img': '../../imgs/paihang3.png',
+      'text': '露营'
+    },
+    {
+      'img': '../../imgs/paihang4.png',
+      'text': '职场'
+    }
+    ],
+    zuohang:[{
+      'img': '../../imgs/paihang5.png',
+      'text': '刘慈欣'
+    },
+      {
+        'img': '../../imgs/paihang6.png',
+        'text': '田小花'
+      },
+      {
+        'img': '../../imgs/paihang7.png',
+        'text': '马尔克斯'
+      },
+      {
+        'img': '../../imgs/paihang8.png',
+        'text': '余秋雨'
+      },
+      {
+        'img': '../../imgs/paihang2.png',
+        'text': '刘慈欣'
+      },
+      {
+        'img': '../../imgs/paihang2.png',
+        'text': '刘慈欣'
+      }
+    ],
+    fabu:[{
+      'img': '../../imgs/kehuan1.png',
+      'text': '三体:死神永生',
+      'actor':'刘慈心'
+    },
+      {
+        'img': '../../imgs/paihang10.png',
+        'text': '人类简史',
+        'actor': '尤瓦力.郝拉利'
+      },
+      {
+        'img': '../../imgs/paihang11.png',
+        'text': '徐三观卖血记',
+        'actor': '余华'
+      },
+      {
+        'img': '../../imgs/paihang12.png',
+        'text': '人类简史',
+        'actor': '刘慈心'
+      },
+      {
+        'img': '../../imgs/paihang13.png',
+        'text': '未来简史',
+        'actor': '尤瓦力.郝拉利'
+      },
+      {
+        'img': '../../imgs/paihang14.png',
+        'text': '百年孤独',
+        'actor': '马尔克斯'
+      }
+    ],
+    rexiao:[{
+      'img':'../../imgs/paihang9.png',
+      'title':'三体:死神永生',
+      'actor':'作者:刘慈心',
+      'content':'刘慈心创作的系列长篇科幻小说地球往事三部曲的第三部作品...'
+    },
+      {
+        'img': '../../imgs/paihang9.png',
+        'title': '三体:死神永生',
+        'actor': '作者:刘慈心',
+        'content': '刘慈心创作的系列长篇科幻小说地球往事三部曲的第三部作品...'
+      },
+      {
+        'img': '../../imgs/paihang9.png',
+        'title': '三体:死神永生',
+        'actor': '作者:刘慈心',
+        'content': '刘慈心创作的系列长篇科幻小说地球往事三部曲的第三部作品...'
+      }]
+  },
+    ketiao: function () {
+      wx.navigateTo({
+        url: "../../pages/kehang/kehang",
+      })
+    },
+})
---
+++ pages/kehang/kehang.js
@@ -0,0 +1,90 @@
+// pages/kehang/kehang.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    rexiao: [{
+      'img': '../../imgs/paihang9.png',
+      'title': '三体:死神永生',
+      'actor': '作者:刘慈心',
+      'content': '刘慈心创作的系列长篇科幻小说地球往事三部曲的第三部作品...'
+    },
+      {
+        'img': '../../imgs/kehuan2.png',
+        'title': '未来简史',
+        'actor': '作者:尤瓦力.郝拉利',
+        'content': '讲述了一段有关未来50年令人难以置信的故事.他剖析了各国...'
+      },
+      {
+        'img': '../../imgs/kehuan3.png',
+        'title': '银河帝国',
+        'actor': '作者:艾萨克.阿西莫夫',
+        'content': '在机器人的帮助下,人类迅速掌握了改造外星球的技术,开启了...'
+      },
+      {
+        'img': '../../imgs/kehuan4.png',
+        'title': '海底两万里',
+        'actor': '作者:儒勒.凡尔纳',
+        'content': '讲述了博物学家阿龙纳斯,其仆人康塞尔和鱼叉手尼德.兰一起随鹦鹉...'
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
